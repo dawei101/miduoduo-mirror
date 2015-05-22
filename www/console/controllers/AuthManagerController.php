7c108502d7ce2cd733a0d6b4b19371d5d7b40560
dawei <liyw@chongdd.com>
2015-05-22 18:13:19 +0800

    fix auth manager command bug

--- www/console/controllers/AuthManagerController.php
+++ www/console/controllers/AuthManagerController.php
@@ -28,7 +28,6 @@ class AuthManagerController extends Controller
 
         $hunter = $auth->createRole('hunter');
         $auth->add($hunter);
-        $manager->addChild($worker);
 
         $dispatcher = $auth->createRole('dispatcher');
         $auth->add($dispatcher);
