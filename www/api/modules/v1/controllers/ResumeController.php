83d8142003af1b55f0151d8112ea9c8fa12ea2e8
dawei <liyw@chongdd.com>
2015-06-02 11:42:02 +0800

    rename resume controller of api

---
+++ www/api/modules/v1/controllers/ResumeController.php
@@ -0,0 +1,15 @@
+<?php
+ 
+namespace api\modules\v1\controllers;
+ 
+use api\modules\BaseActiveController;
+ 
+/**
+ * Resume Controller API
+ *
+ * @author dawei
+ */
+class ResumeController extends BaseActiveController
+{
+    public $modelClass = 'common\models\Resume';
+}
