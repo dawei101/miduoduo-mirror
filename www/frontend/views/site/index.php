5280c10f83dd96954ca5d14c24952e7f0537af4a
boolean-wang <boolean.wang@foxmail.com>
2015-06-17 14:10:00 +0800

    delete some link

--- www/frontend/views/site/index.php
+++ www/frontend/views/site/index.php
@@ -16,7 +16,7 @@ $this->title = '米多多兼职平台';
   <div class="top-center">
     <nav>
       <div class="logo"><img src="img/logo.png" width="200" height="110"></div>
-      <div class="nav"><a href="/site/Contact/"  class="contact-us">联系我们</a><a href="#">企业合作</a></div>
+      <!-- <div class="nav"><a href="/site/Contact/"  class="contact-us">联系我们</a><a href="#">企业合作</a></div> -->
     </nav>
     <div class="banner"><img src="img/banner.png" width="751" height="407">
       <h1>米多多 | 大学生兼职平台</h1>
