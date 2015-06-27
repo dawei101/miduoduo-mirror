d745be7913559e97c9295134d5d3d5e41bff6d0e
dawei <yanwei.li.24@gmail.com>
2015-06-28 01:33:41 +0800

    build spider struct

---
+++ spider/mspider/items.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Record(scrapy.Item):
+
+    title = scrapy.Field()
+    category_name = scrapy.Field()
+    is_long_term = scrapy.Field()
+    from_date = scrapy.Field()
+    to_date = scrapy.Field()
+    from_time = scrapy.Field()
+    to_time = scrapy.Field()
+    address = scrapy.Field()
+    content = scrapy.Field()
+    need_quantity = scrapy.Field()
+    salary = scrapy.Field()
+    salary_unit = scrapy.Field()
+    clearance_period = scrapy.Field()
+    company_name = scrapy.Field()
+    contact = scrapy.Field()
+    phonenum = scrapy.Field()
+    email = scrapy.Field()
+    release_time = scrapy.Field()
---
+++ spider/mspider/spiders/jianzhimao_spider.py
@@ -0,0 +1,18 @@
+
+
+
+import scrapy
+
+class WubaSpider(scrapy.Spider):
+
+    name = "wuba"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
