8018fd3b5b26802e4fc9c20a5b82ca8dd0e9ff45
dawei <yanwei.li.24@gmail.com>
2015-07-04 05:19:13 +0800

    重套详情页，添加jubao

---
+++ www/backend/views/complaint/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\ComplaintSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Complaints';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="complaint-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Complaint', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'content:ntext',
+            'task_id',
+            'user_id',
+            // 'created_time',
+            // 'status',
+            // 'phonenum',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
