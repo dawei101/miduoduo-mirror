5c2c02beb08e5745f4af32dc73289dd1b74f5513
GY <229043501@QQ.COM>
2015-07-04 09:11:33 +0800

    * util

--- www/html5_src/js/widget/util.js
+++ www/html5_src/js/widget/util.js
@@ -5,7 +5,22 @@ define(function(require, exports) {
         }
     }
 
-    function apiGen() {
+    function app(option, callback) {
+        WebViewJavascriptBridge.send(json, callback);
+    }
+
+
+    function href(_url) {
+        if (!miduoduo.os.mddApp) {
+            location.href = url;
+        } else {
+            var opts = {
+                action: 'b_push',
+                data: {url: _url}
+            }
+            app(opts, null);
+        }
 
     }
+
 });
