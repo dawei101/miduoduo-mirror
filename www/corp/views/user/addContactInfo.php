279474e58c3edd48a2c94b33110f2fcc979f8aef
怀柳 <quafferences@gmail.com>
2015-07-04 17:24:38 +0800

    reindent

--- www/corp/views/user/addContactInfo.php
+++ www/corp/views/user/addContactInfo.php
@@ -9,79 +9,61 @@ use yii\bootstrap\ActiveForm;
 $this->title = '开通招聘服务';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>1. fill in contact info</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'add-contact-info-form']); ?>
-                <?= $form->field($model, 'name')?>
-                <?= $form->field($model, 'phone') ?>
-                <?= $form->field($model, 'email') ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-</div>
 
 <!-- InstanceBeginEditable name="EditRegion3" -->
 <div class="midd-kong"></div>
 <div class="container mima">
-  <div class="row">
-    <div class="col-sm-12 col-md-12 col-lg-12 ">
-      <div class="miam-coter">
-        <h2>开通招聘服务</h2>
-        <div class="box-jindu">
-            <div class="one1"></div>
-            <div class="one-n">1</div>
-            <div class="two-n1">2</div>
-            <div class="three-n1">3</div>
-            <div class="one-text">填写联系方式</div>
-            <div class="two-text1">提交公司信息</div>
-             <div class="three-text1">发布职位</div>
-        </div>
-        <form action="/user/add-contact-info" method="post">
-            <div class="tx-box">
-          <span class="pull-left text-right">企业名称 </span>
-          <div class="midd-input-group pull-left">
-          <input name="name" type="text" class="input-q"  placeholder="准确填写公司名，提升投递量">
-          </div>
-          <em class="pull-right">*</em>
-         </div>
-         <div class="tx-box">
-          <span class="pull-left text-right">联系人 </span>
-          <div class="midd-input-group pull-left">
-          <input name="contact" type="text" class="input-q"  placeholder="负责招聘的联系人姓名">
-          </div>
-          <em class="pull-right">*</em>
-         </div>
-        <div class="tx-box">
-          <span class="pull-left text-right">公司联系电话 </span>
-          <div class="midd-input-group pull-left">
-          <input name="phone" type="text" class="input-q"  placeholder="请填写真实有效手机/座机号码">
-          </div>
-          <em class="pull-right">*</em>
-         </div>
-         <div class="tx-box">
-          <span class="pull-left text-right"> 接收简历邮箱 </span>
-          <div class="midd-input-group pull-left">
-            <input name="email" type="text" class="input-q"  placeholder="请填写公司邮箱，审核通过后不可更改">
-          </div>
-          <em class="pull-right">*</em>
-          </div>
-          <div class="tx-box">
-          <span class="pull-left text-right"></span>
-            <a href="#" class="zc-btn pull-right">确定</a>
-          </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 ">
+            <div class="miam-coter">
+                <h2>开通招聘服务</h2>
+                <div class="box-jindu">
+                    <div class="one1"></div>
+                    <div class="one-n">1</div>
+                    <div class="two-n1">2</div>
+                    <div class="three-n1">3</div>
+                    <div class="one-text">填写联系方式</div>
+                    <div class="two-text1">提交公司信息</div>
+                    <div class="three-text1">发布职位</div>
+                </div>
+                <form action="/user/add-contact-info" method="post">
+                    <div class="tx-box">
+                        <span class="pull-left text-right">企业名称 </span>
+                        <div class="midd-input-group pull-left">
+                            <input name="name" type="text" class="input-q"  placeholder="准确填写公司名，提升投递量">
+                        </div>
+                        <em class="pull-right">*</em>
+                    </div>
+                    <div class="tx-box">
+                        <span class="pull-left text-right">联系人 </span>
+                        <div class="midd-input-group pull-left">
+                            <input name="contact" type="text" class="input-q"  placeholder="负责招聘的联系人姓名">
+                        </div>
+                        <em class="pull-right">*</em>
+                    </div>
+                    <div class="tx-box">
+                        <span class="pull-left text-right">公司联系电话 </span>
+                        <div class="midd-input-group pull-left">
+                            <input name="phone" type="text" class="input-q"  placeholder="请填写真实有效手机/座机号码">
+                        </div>
+                        <em class="pull-right">*</em>
+                    </div>
+                    <div class="tx-box">
+                        <span class="pull-left text-right"> 接收简历邮箱 </span>
+                        <div class="midd-input-group pull-left">
+                            <input name="email" type="text" class="input-q"  placeholder="请填写公司邮箱，审核通过后不可更改">
+                        </div>
+                        <em class="pull-right">*</em>
+                    </div>
+                    <div class="tx-box">
+                        <span class="pull-left text-right"></span>
+                        <a href="#" class="zc-btn pull-right">确定</a>
+                    </div>
 
-        </form>
-      </div>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <!-- InstanceEndEditable -->
