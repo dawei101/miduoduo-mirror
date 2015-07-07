ceba7dc3b3597359cd852fba3e5d8c0fcbe89a73
huailiu <quafferences@gmail.com>
2015-07-07 08:18:02 +0800

    update

---
+++ www/corp/controllers/ResumeController.php
@@ -0,0 +1,56 @@
+<?php
+namespace corp\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use corp\FBaseController;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+
+/**
+ * Site controller
+ */
+class SiteController extends FBaseController
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                    'login' => ['post'],
+                    'register' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this -> render('index');
+    }
+
+
+}
