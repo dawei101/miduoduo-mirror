bf46c13414e98c2e6d3a67a53d1c45e5f60d0e70
dincode <quafferences@gmail.com>
2015-07-08 23:52:26 +0800

    update

--- www/corp/models/TaskPublishModel.php
+++ www/corp/models/TaskPublishModel.php
@@ -37,49 +37,10 @@ class TaskPublishModel extends Model
     public function rules()
     {
         return [
+             [['title', '$salary'], 'required'],
         ];
     }
 
-    public function attributeLabels()
-    {
-        return [
-            'username' => '手机号',
-            'password' => '密码',
-            'rememberMe' => '记住我',
-        ];
-    }
-
-
-    /**
-     * Validates the password.
-     * This method serves as the inline validation for password.
-     *
-     * @param string $attribute the attribute currently being validated
-     * @param array $params the additional name-value pairs given in the rule
-     */
-    public function validatePassword($attribute, $params)
-    {
-        if (!$this->hasErrors()) {
-            $user = $this->getUser();
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, '密码错误');
-            }
-        }
-    }
-
-    /**
-     * Logs in a user using the provided username and password.
-     *
-     * @return boolean whether the user is logged in successfully
-     */
-    public function login()
-    {
-        if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
-        } else {
-            return false;
-        }
-    }
 
     /**
      * Finds user by [[username]]
