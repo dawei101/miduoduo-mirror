8a76e75a557bb5267b5dd2ff9db8391a340d0eaa
dawei <liyw@chongdd.com>
2015-07-08 17:44:34 +0800

    modify sms template

--- www/common/pusher/sms_tpl/to-applicant-task-applied-done.php
+++ www/common/pusher/sms_tpl/to-applicant-task-applied-done.php
@@ -1,1 +1,1 @@
-<?=$resume->name?>，您已应聘【<?=mb_substr($task->tidyTitle(), 0, 6, 'utf-8')?>】，简历已到达企业。招聘电话:<?=$task->contact?><?=$task->contact_phonenum?>。如遇问题，致电米多多：84856897
+【米多多】<?=$resume->name?>，您已应聘"<?=mb_substr($task->tidyTitle(), 0, 6, 'utf-8')?>"，简历已到达企业。招聘电话:<?=$task->contact?><?=$task->contact_phonenum?>。如遇问题，致电米多多：84856897
