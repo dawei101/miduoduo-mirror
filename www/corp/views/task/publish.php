3a64b4041384e9485b5225f6ab9a1e6a9d80b1a7
dincode <quafferences@gmail.com>
2015-07-14 07:57:01 +0800

    test

--- www/corp/views/task/publish.php
+++ www/corp/views/task/publish.php
@@ -270,13 +270,6 @@ $this->title = '米多多兼职平台';
             </div>
         </div>
     </div>
-<script src="/js/miduoduo.js"></script>
-<script src="/js/data/moment.js"></script>
-<script src="/js/data/daterangepicker.js"></script>
-
-<script src="/js/fuwenben/bootstrap-wysiwyg.js"></script>
-<script src="/js/fuwenben/external/jquery.hotkeys.js"></script>
-<script src="/js/jquery.tagbox.js"></script>
 <!-- InstanceEndEditable -->
 <?php
 $this->registerJsFile('/js/publish.js');
