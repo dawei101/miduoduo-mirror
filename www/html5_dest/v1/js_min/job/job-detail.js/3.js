5d9fe4662b4f30ab6465bd4d0842b210be095899
dawei <liyw@chongdd.com>
2015-07-14 20:42:13 +0800

    delete h5 dest

--- www/html5_dest/v1/js_min/job/job-detail.js/3.js
+++
@@ -1,1 +0,0 @@
-define(function(require,exports){require("zepto");var tpl=require("../widget/tpl-engine"),api=require("../widget/api");$.getJSON(api.gen("task/143394256310412"),function(data){console.log(data),$("body").append(tpl.parse("main-tpl",{data:data}))})});
