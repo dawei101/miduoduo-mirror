4ba23999efe25b51225bd3dcee9766b683c9dbd3
dincode <quafferences@gmail.com>
2015-07-15 02:45:53 +0800

    update

--- www/corp/web/js/publish.js
+++ www/corp/web/js/publish.js
@@ -7,3 +7,7 @@ $(function() {
 $('form').on('submit', function(){
     $("[name='detail']")[0].value = $("#editor").html();
 });
+
+$("#address_count").on('change', function(){
+    console.log($(this).value);
+});
