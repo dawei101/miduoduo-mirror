00088d62ae4881ff3e99a87ee293811059109ef1
dincode <quafferences@gmail.com>
2015-07-15 10:40:58 +0800

    fix bug

--- www/corp/views/layouts/main.php
+++ www/corp/views/layouts/main.php
@@ -40,24 +40,16 @@ AppAsset::register($this);
                 <a class="navbar-brand" href="/"><img src="/img/qiye-logo.png"></a> </div>
                 <div class="collapse navbar-collapse" id="example-navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <?php if(!Yii::$app->user->isGuest){?>
-                            <li class="active"><a href="/task/">我的职位</a></li>
-                            <li><a href="/task/publish">我要发布</a></li>
-                            <li><a href="/resume/">简历管理</a></li>
-                            <li><a href="/site/message">消息
-                                <!--
-                                <em style="background:#fed732  ; border-radius:20px; padding:0 10px;  color:#fff; z-index:40">1</em>
-                                -->
-                            </a></li>
-                            <li><a href="/user/info">用户中心</a></li>
-                            <li><a href="/user/logout">退出</a></li>
-                        <?php }else{?>
-                            <li class="active"><a href="#">我的职位</a></li>
-                            <li><a href="#">我要发布</a></li>
-                            <li><a href="#">简历管理</a></li>
-                            <li><a href="#">消息</a></li>
-                            <li><a href="#">用户中心</a></li>
-                        <?php }?>
+                        <li class="active"><a href="/task/">我的职位</a></li>
+                        <li><a href="/task/publish">我要发布</a></li>
+                        <li><a href="/resume/">简历管理</a></li>
+                        <li><a href="/site/message">消息
+                            <!--
+                            <em style="background:#fed732  ; border-radius:20px; padding:0 10px;  color:#fff; z-index:40">1</em>
+                            -->
+                        </a></li>
+                        <li><a href="/user/info">用户中心</a></li>
+                        <li><a href="/user/logout">退出</a></li>
                     </ul>
                 </div>
             </div>
