33c51b7ab4be8c98f0b44cb91652187b21f3ca9b
dincode <quafferences@gmail.com>
2015-07-15 10:30:37 +0800

    Revert "update"
    This reverts commit 4ba23999efe25b51225bd3dcee9766b683c9dbd3.

--- www/corp/web/js/publish.js
+++ www/corp/web/js/publish.js
@@ -7,7 +7,3 @@ $(function() {
 $('form').on('submit', function(){
     $("[name='detail']")[0].value = $("#editor").html();
 });
-
-$("#address_count").on('change', function(){
-    console.log($(this).value);
-});
