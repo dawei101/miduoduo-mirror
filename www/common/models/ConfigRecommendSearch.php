0fcd7f546a801f4ce916ba9ecb2854ab05fb52a7
dawei <yanwei.li.24@gmail.com>
2015-07-17 02:09:30 +0800

    change default order of dashboard, add new search features

--- www/common/models/ConfigRecommendSearch.php
+++ www/common/models/ConfigRecommendSearch.php
@@ -45,6 +45,7 @@ class ConfigRecommendSearch extends ConfigRecommend
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => ['defaultOrder'=>['id'=>SORT_DESC]],
         ]);
 
         $this->load($params);
--- www/common/models/DistrictSearch.php
+++ www/common/models/DistrictSearch.php
@@ -45,6 +45,7 @@ class DistrictSearch extends District
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => ['defaultOrder'=>['id'=>SORT_DESC]],
         ]);
 
         $this->load($params);
