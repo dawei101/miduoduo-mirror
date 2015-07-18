0cd045b0aaea9d90a4c83104d55a94bdbf33fe81
dawei <liyw@chongdd.com>
2015-07-18 15:48:02 +0800

    company status

--- www/console/controllers/TaskController.php
+++ www/console/controllers/TaskController.php
@@ -20,6 +20,7 @@ class TaskController extends Controller
             'to_date < :to_date and status=:status' , 
             ['to_date' => $now, 'status' => Task::STATUS_OK]
         );
+        echo "Task Status:: $count tasks are changed to offline\n";
         Yii::info("Task Status:: $count tasks are changed to offline");
     }
 }
