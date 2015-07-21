91cb48ad1cf7c5e99b0956718f1332c7a0f2eeba
huailiu <quafferences@gmail.com>
2015-07-21 14:58:27 +0800

    fix bug

--- www/common/models/Task.php
+++ www/common/models/Task.php
@@ -163,7 +163,7 @@ class Task extends \common\BaseActiveRecord
         return [
             [['salary', 'salary_unit', 'from_date', 'to_date',
                 'need_quantity', 'detail',
-                'title'], 'required'],
+                'title', 'service_type_id'], 'required'],
             ['company_id', 'required', 'message'=>'请选择一个已存在的公司'],
             [['id', 'clearance_period', 'salary_unit', 'need_quantity',
                 'got_quantity', 'user_id', 'service_type_id',
