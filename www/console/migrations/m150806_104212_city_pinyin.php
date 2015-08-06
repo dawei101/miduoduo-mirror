948ccf80674ce17678b474ea7f77417c452d2975
dawei <liyw@chongdd.com>
2015-08-06 20:10:30 +0800

    add pinyin trans lib to trans city name to pinyin

--- www/console/migrations/m150806_104212_city_pinyin.php
+++ www/console/migrations/m150806_104212_city_pinyin.php
@@ -3,6 +3,7 @@
 use yii\db\Schema;
 use yii\db\Migration;
 use common\models\District;
+use common\pinyin\Pinyin;
 
 class m150806_104212_city_pinyin extends Migration
 {
@@ -12,6 +13,8 @@ class m150806_104212_city_pinyin extends Migration
         $zsstr = "蒙古族、回族、藏族、维吾尔族、苗族、彝族、壮族、布依族、朝鲜族、满族、侗族、瑶族、白族、土家族、哈尼族、哈萨克族、傣族、黎族、僳僳族、佤族、畲族、高山族、拉祜族、水族、东乡族、纳西族、景颇族、柯尔克孜族、土族、达斡尔族、仫佬族、羌族、布朗族、撒拉族、毛南族、仡佬族、锡伯族、阿昌族、普米族、塔吉克族、怒族、乌孜别克族、俄罗斯族、鄂温克族、德昂族、保安族、裕固族、京族、塔塔尔族、独龙族、鄂伦春族、赫哲族、门巴族、珞巴族、基诺族、傈僳族";
 
         $rezs = '/(' . implode('|', explode('、', $zsstr)) . ')/u';
+        Pinyin::set('delimiter', ' ');
+        Pinyin::set('accent', 0);
         foreach (District::find()->all() as $d){
             if (!empty($d->pinyin)){
                 continue;
@@ -27,15 +30,8 @@ class m150806_104212_city_pinyin extends Migration
                 $d->short_name = $d->name;
             }
             print $d->name . "\n";
-            $r = file_get_contents('http://string2pinyin.sinaapp.com/?str='. urlencode($d->short_name) .'&accent=0');
-            $json = json_decode($r);
-            if ($json){
-                $d->pinyin = $json->pinyin;
-                $d->short_pinyin = '';
-                foreach (explode(' ', $json->pinyin) as $v){
-                    $d->short_pinyin .= substr($v, 0, 1);
-                }
-            }
+            $d->pinyin = Pinyin::trans($name);
+            $d->short_pinyin = str_replace(' ', '', Pinyin::letter($name));
 
             print $name . "\t\t>>\t"  . $d->pinyin ." + " . $d->short_pinyin . " >> \t\t" . $d->name . "\n";
             $d->save();
