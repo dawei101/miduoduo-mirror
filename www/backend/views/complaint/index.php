a4c3f2a24f420a7a9e8a50e5078d796716f2ffc9
suibber <suibber@gmail.com>
2015-08-11 18:28:04 +0800

    fix bug

--- www/backend/views/complaint/index.php
+++ www/backend/views/complaint/index.php
@@ -34,6 +34,8 @@ $this->params['breadcrumbs'][] = $this->title;
                 'value' => function($model){
                     if(isset($model->task->gid)){
                         return "<a target='_blank' href='" . \Yii::$app->params['baseurl.m'] . "/task/view/?gid=" . $model->task->gid ."'>" . $model->task->title . "</a>";
+                    }else{
+                        return '无';
                     }
                 }
             ] ,
