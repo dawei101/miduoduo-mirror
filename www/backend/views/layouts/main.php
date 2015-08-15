99de37ea68498d11872faedd4adeddcecbc52aad
dawei <liyw@chongdd.com>
2015-08-15 15:50:41 +0800

    折叠面板菜单

--- www/backend/views/layouts/main.php
+++ www/backend/views/layouts/main.php
@@ -26,41 +26,50 @@ AppAsset::register($this);
     <div id="wrapper">
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
-              <ul class="sidebar-nav">
+              <ul id="sidebar" class="sidebar-nav">
                 <li class="sidebar-brand">
                     <a href="/"> 米多多 </a>
                 </li>
-                <li><a href="/user">账号管理</a></li>
-                <li><a href="/resume">人才库</a></li>
-                <li><a href="/company">企业库</a></li>
-                <li><a href="/district">城市</a></li>
-                <li><a href="/service-type">任务类型</a></li>
-                <li><a href="/task">任务</a></li>
-                <li><a href="/task-applicant">任务报名单</a></li>
-                <li><a href="/complaint">投诉列表</a></li>
-                <li><a href="/contact-us">联系我们的客户</a></li>
-                <li><a href="/config-recommend">后台配置-推荐兼职</a></li>
-                <li>&nbsp;</li>
-                <li><a href="/weichat-push-set-pushset">微信推送-附近</a></li>
-                <li><a href="/weichat-push-set-template-push-list">微信推送-附近-模板</a></li>
-                <li><a href="/weichat-push-quality-task">微信推送-优单</a></li>
-                <li><a href="/weichat-user-info">微信推送-绑定用户</a></li>
-                <li><a href="/weichat-erweima/index?sort=-id">微信二维码</a></li>
-                <li><a href="/weichat-autoresponse">微信自动回复</a></li>
-                <li>&nbsp;</li>
-                <li><a href="/data-user">数据统计</a></li>
-                <li>&nbsp;</li>
-                <li><a href="/account-event-cache">工资流水-待发工资</a></li>
-                <li><a href="/account-event">工资流水-已发工资</a></li>
-                <li><a href="/withdraw-cash">提现流水</a></li>
-                <li><a href="/user-account">用户资金</a></li>
-                <li>&nbsp;</li>
+                <li class="mtitle" data-group="admin"><a>账号，简历，任务分类</a></li>
+                <li class="mmenu" data-group="admin"><a href="/user">账号管理</a></li>
+                <li class="mmenu" data-group="admin"><a href="/resume">人才库</a></li>
+                <li class="mmenu" data-group="admin"><a href="/district">城市</a></li>
+                <li class="mmenu" data-group="admin"><a href="/service-type">任务类型</a></li>
 
-                <li><a href="/task-pool">爬虫-任务列表</a></li>
-                <li><a href="/task-pool-white-list">爬虫-白名单(黑名单)</a></li>
-                <li>&nbsp;</li>
-                <li><a href="/app-release-version">应用发布管理</a></li>
-                <li><a href="/job-queue">异步任务</a></li>
+                <li class="mtitle" data-group="operation"><a>信息运营</a></li>
+                <li class="mmenu" data-group="operation"><a href="/task">任务</a></li>
+                <li class="mmenu" data-group="operation"><a href="/task-applicant">任务报名单</a></li>
+                <li class="mmenu" data-group="operation"><a href="/task-pool">爬虫-任务列表</a></li>
+                <li class="mmenu" data-group="operation"><a href="/task-pool-white-list">爬虫-白名单(黑名单)</a></li>
+                <li class="mmenu" data-group="operation"><a href="/company">企业库</a></li>
+                <li class="mmenu" data-group="operation"><a href="/config-recommend">推荐兼职-配置</a></li>
+
+                <li class="mtitle" data-group="wechat"><a>微信</a></li>
+                <li class="mmenu" data-group="wechat"><a href="/weichat-push-set-pushset">微信推送-附近</a></li>
+                <li class="mmenu" data-group="wechat"><a href="/weichat-push-set-template-push-list">微信推送-附近-模板</a></li>
+                <li class="mmenu" data-group="wechat"><a href="/weichat-push-quality-task">微信推送-优单</a></li>
+                <li class="mmenu" data-group="wechat"><a href="/weichat-user-info">微信推送-绑定用户</a></li>
+                <li class="mmenu" data-group="wechat"><a href="/weichat-erweima/index?sort=-id">微信二维码</a></li>
+                <li class="mmenu" data-group="wechat"><a href="/weichat-autoresponse">微信自动回复</a></li>
+
+                <li class="mtitle" data-group="finance"><a>资金账户管理</a></li>
+                <li class="mmenu" data-group="finance"><a href="/account-event-cache">工资流水-待发工资</a></li>
+                <li class="mmenu" data-group="finance"><a href="/account-event">工资流水-已发工资</a></li>
+                <li class="mmenu" data-group="finance"><a href="/withdraw-cash">提现流水</a></li>
+                <li class="mmenu" data-group="finance"><a href="/user-account">用户资金</a></li>
+
+                <li class="mtitle" data-group="feedback"><a>用户反馈</a></li>
+                <li class="mmenu"  data-group="feedback"><a href="/complaint">投诉列表</a></li>
+                <li class="mmenu"  data-group="feedback"><a href="/contact-us">联系我们的客户</a></li>
+
+                <li class="mtitle" data-group="analytics"><a>统计</a></li>
+                <li class="mmenu" data-group="analytics"><a href="/data-user">数据统计</a></li>
+
+                <li class="mtitle" data-group="dev"><a>产品研发</a></li>
+                <li class="mmenu" data-group="dev"><a href="/app-release-version">应用发布管理</a></li>
+                <li class="mmenu" data-group="dev"><a href="/job-queue">异步任务</a></li>
+                <li class="mmenu" data-group="dev"><a href="/user/delete-myself" data-method="post" data-confirm="删除后所有的数据将被清掉，确定删除?">自杀</a></li>
+
                 <li class="hidden">
                     <a href="/support/report-bug">
                     <span class="glyphicon glyphicon-flag" style="color:red;"></span> 提交bug
@@ -70,8 +79,6 @@ AppAsset::register($this);
 <?php if (Yii::$app->user->isGuest) { ?>
                 <li><a href="/site/login">登陆</a></li>
 <?php } else { ?>
-                <li><a href="/user/delete-myself" data-method="post" data-confirm="删除后所有的数据将被清掉，确定删除?">自杀</a></li>
-                <li>&nbsp;</li>
                 <li><a href="/site/logout" data-method="post">退出</a></li>
 <?php } ?>
 
@@ -97,12 +104,20 @@ AppAsset::register($this);
         $(function(){
             var uri=location.pathname;
             console.info(uri);
+
             $.each($('#sidebar a'), function(i, v){
                 var a =$(v);
                 var muri = a.attr('href');
                 if (uri==muri){
-                    a.closest('li').addClass('active');
+                    var li = a.closest('li');
+                    li.addClass('active');
+                    $(".mmenu").hide();
+                    $("[data-group='" + li.attr('data-group') +"']").show();
                 }
+            });
+            $(".mtitle").click(function(){
+                $("[data-group='"+ $(this).attr('data-group') +"']").toggle();
+                $(this).toggle();
             });
         });
 
