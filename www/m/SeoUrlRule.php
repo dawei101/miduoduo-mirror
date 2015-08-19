ba444a110d0c5e70aff7d20ed3dcfffc8a07c348
dawei <liyw@chongdd.com>
2015-08-19 16:41:26 +0800

    seo rul

--- www/m/SeoUrlRule.php
+++ www/m/SeoUrlRule.php
@@ -12,92 +12,62 @@ use common\models\ServiceType;
 
 class SeoUrlRule extends UrlRule
 {
-
     public function parseRequest($manager, $request)
     {
-        $path    = $request->getPathInfo();
-        $url     = $request->getUrl();
-        if( stripos($url,'?') || !$path ){
-            return false;
-        }
+        $path = $request->pathInfo;
 
-        $result  = Yii::$app->cache->get($path);
-        //$result = '';
-        if( !$result ){
-            $path_arr   = explode('/',$path);
-            $path_city  = isset($path_arr[0]) ? $path_arr[0] : 'beijing';
-            if( $path_city == 'task' ){
-                $path_city = 'beijing';
-            }
-            $path_two   = isset($path_arr[1]) ? $path_arr[1] : '';
-            $path_three = isset($path_arr[2]) ? $path_arr[2] : '';
-            $city_id    = '3';
-            $districts_id = '';
-            $type_id    = '';
-            $city_pinyin    = 'beijing';
-            $districts_pinyin = '';
-            $type_pinyin    = '';
+        $key = 'task_list_url_match_rules';
+        $re = Yii::$app->cache->get($key);
 
+        if (!$re){
             $cities = District::findAll(['level'=>'city', 'is_alive'=>1]);
             $cs_pinyins= [];
             foreach($cities as $city){
-                $cs_pinyins[$city->id] = $city->seo_pinyin;
+                $cs_pinyins[] = $city->seo_pinyin;
             }
-            if( $path_city && $city_id = array_search($path_city,$cs_pinyins) ){
-                $city_pinyin= $path_city;
-                if( $path_two != 'task' ){
-                    $districts  = District::findAll(['level'=>'district','parent_id'=>$city_id, 'is_alive'=>1]);
-                    $ds_pinyins = [];
-                    foreach($districts as $district){
-                        $ds_pinyins[$district->id] = $district->seo_pinyin;
-                    }
-                    if( $path_two && $districts_id = array_search($path_two,$ds_pinyins) ){
-                        $districts_pinyin = $path_two;
-                        // 有第二个参数，并且为区
-                        $stypes     = ServiceType::findAll(['status'=>0]);
-                        $ts_pinyin  = [];
-                        foreach( $stypes as $type ){
-                            $ts_pinyin[$type->id] = $type->pinyin;
-                        }
-                        if( $path_three && $type_id = array_search($path_three,$ts_pinyin) ){
-                            $type_pinyin = $path_three;
-                            // 有第三个参数，并且为类型
-                        }else{
-                            // 无第三个参数
-                        }
-                    }else{
-                        $stypes     = ServiceType::findAll(['status'=>0]);
-                        $ts_pinyin  = [];
-                        foreach( $stypes as $type ){
-                            $ts_pinyin[$type->id] = $type->pinyin;
-                        }
-                        if( $path_two && $type_id = array_search($path_two,$ts_pinyin) ){
-                            $type_pinyin = $path_two;
-                            // 有第二个参数，并且为类型
-                        }else{
-                            // 无第二个参数
-                        }
-                    }
-                }
-            }else{
-                return false;
-            }
+            $city_re = implode('|', $cs_pinyins);
+            $districts  = District::findAll(
+                ['level'=>'district', 'is_alive'=>1]);
 
-            $result = [
+            $districts_pinyins = [];
+            foreach($districts as $district){
+                $districts_pinyins[] = $district->seo_pinyin;
+            }
+            $district_re = implode('|', array_unique($districts_pinyins));
+
+            $stypes     = ServiceType::findAll(['status'=>0]);
+            $ts_pinyin  = [];
+            foreach( $stypes as $type ){
+                $ts_pinyin[] = $type->pinyin;
+            }
+            $s_re = implode('|', $ts_pinyin);
+
+            $re = '/^(task|('.$city_re.'))\/(('.$district_re.')\/)?(('.$s_re.')\/)?$/i';
+            Yii::$app->cache->set($key, $re, 60*60*3);
+        }
+        if (preg_match($re, $path, $matches)){
+            $city_pinyin = 'beijing';
+            $district_pinyin = '';
+            $type_pinyin = '';
+            if (isset($matches[2]) && $matches[2]!='task'){
+                $city_pinyin = $matches[2];
+            }
+            if (count($matches)>4){
+                $district_pinyin = $matches[4];
+            }
+            if (count($matches)>6){
+                $type_pinyin = $matches[6];
+            }
+            return [
                 0   => 'task/index',
                 1   => [
-                    'city_id'       => $city_id,
-                    'districts_id'  => $districts_id,
-                    'type_id'       => $type_id,
-                    'city_pinyin'       => $city_pinyin,
-                    'districts_pinyin'  => $districts_pinyin,
-                    'type_pinyin'       => $type_pinyin,
+                    'city_pinyin' => $city_pinyin,
+                    'district_pinyin'=> $district_pinyin,
+                    'type_pinyin' => $type_pinyin,
                 ],
             ];
-            
-            Yii::$app->cache->add($path,$result);
         }
-        return $result;
+        return false;
     }
 
 }
