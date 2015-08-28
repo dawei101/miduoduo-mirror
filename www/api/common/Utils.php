1098cbb6a2e99b3206954a9b026516d2ecb39075
dawei <liyw@chongdd.com>
2015-08-28 15:56:21 +0800

    api hot fix :Utils

--- www/api/common/Utils.php
+++ www/api/common/Utils.php
@@ -19,7 +19,7 @@ class Utils
             'has_resume' => !empty($user->resume),
             'wechat' => !empty($user->weichat),
             'has_wechat' => !empty($user->resume),
-            'id_examed' => $user->resume?($this->resume->exam_status==Resume::EXAM_DONE):false,
+            'id_examed' => $user->resume?($user->resume->exam_status==Resume::EXAM_DONE):false,
             'last_city' => [],
         ];
         if ($user->last_location){
