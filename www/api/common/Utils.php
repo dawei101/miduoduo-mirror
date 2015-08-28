68b05ceacd489534d363cd0ae0bd19e5b4d99ce2
dawei <liyw@chongdd.com>
2015-08-28 15:16:34 +0800

    api: user->profile :: id_examed

--- www/api/common/Utils.php
+++ www/api/common/Utils.php
@@ -1,6 +1,7 @@
 <?php
 namespace api\common;
 use common\models\User;
+use common\models\Resume;
 
 
 class Utils
@@ -18,6 +19,7 @@ class Utils
             'has_resume' => !empty($user->resume),
             'wechat' => !empty($user->weichat),
             'has_wechat' => !empty($user->resume),
+            'id_examed' => $user->resume?($this->resume->exam_status==Resume::EXAM_DONE):false,
             'last_city' => [],
         ];
         if ($user->last_location){
