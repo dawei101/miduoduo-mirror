f0c991b8a32d6ebb9280ef5d4e02fb6f06accb6c
suibber <suibber@gmail.com>
2015-09-10 18:03:18 +0800

    fix | 增加对jz_company里公司名的修改

--- www/console/migrations/m150910_082603_update_company_name.php
+++ www/console/migrations/m150910_082603_update_company_name.php
@@ -8,7 +8,8 @@ class m150910_082603_update_company_name extends BaseMigration
     public function up()
     {
         $sqls = "
-            UPDATE `jz_task` SET company_name='米多多运营中心' WHERE company_name LIKE '%校联邦%'
+            UPDATE `jz_task` SET company_name='米多多运营中心' WHERE company_name LIKE '%校联邦%';
+            UPDATE `jz_company` SET `name`='米多多运营中心' WHERE `name` LIKE '%校联邦%';
             ";
         return $this->execSqls($sqls);
     }
